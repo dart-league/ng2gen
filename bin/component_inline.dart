3b1774399916b11039e08d832bdf2e3a22e6784d
luis <luisbigvargas@gmail.com>
2016-01-30 15:35:35 -0500

    edit component_inline to use """

--- bin/component_inline.dart
+++ bin/component_inline.dart
@@ -5,16 +5,18 @@ main(List<String> args) async {
 //  await new Directory('${args[0]}').create();
   var name = args[0];
   (await new File('${name}.dart').create()).writeAsString(
-      '''// Copyright (c) 2016, <your name>. All rights reserved. Use of this source code
+      """// Copyright (c) 2016, <your name>. All rights reserved. Use of this source code
 // is governed by a BSD-style license that can be found in the LICENSE file.
 
 import 'package:angular2/angular2.dart';
 
 @Component(
   selector: '${toPolyName(name)}',
-  template: '')
+  template: '''
+
+  ''')
 class ${toUpperCamelCase(name)} {
 
 }
-''');
+""");
 }
